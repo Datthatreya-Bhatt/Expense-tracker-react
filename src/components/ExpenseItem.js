b8fcaad6612539c3391d69f5e9736323ad8b9739
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-09-16 20:56:58 +0530

    variables created in react

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,22 @@
-
+import "./ExpenseItem.css"
 
 function ExpenseItem(){
+
+    const expenseDate = new Date(2021, 2 ,28);
+    const expenseTitle = 'Car insurence';
+    const expenseAmount = 100;
+    const expenseLocation = 'Banglore'
+
+
     return (
-        <div>
-            <h2>Expense Item</h2>
-            <h2>Food Rs 10</h2>
-            <h2>Petrol Rs 100</h2>
-            <h2>Movies Rs 200</h2>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <h2>{expenseLocation}</h2>
+
+                <div className="expense-item__price">${expenseAmount}</div>
+            </div>
         </div>
     )
 }
