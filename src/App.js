8ca8f4ae267d1c933ada33c4c4e82f8e5dd55ebd
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-09-16 21:22:42 +0530

    props created and data passed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,45 @@ import ExpenseItem from "./components/ExpenseItem";
 
 
 function App() {
+
+  const expense = [
+    {id: '1', title: 'Abc', amount: 100, date: new Date(2021, 2,28), location: 'banglore'},
+    {id: '2', title: 'efg', amount: 100, date: new Date(2021, 2,28), location: 'banglore'},
+    {id: '3', title: 'hij', amount: 100, date: new Date(2021, 2,28), location: 'banglore'},
+    {id: '4', title: 'klm', amount: 100, date: new Date(2021, 2,28), location: 'banglore'},
+  ]
+
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem 
+      title= {expense[0].title}
+      amount={expense[0].amount}
+      date={expense[0].date}
+      location={expense[0].location}
+      ></ExpenseItem>
+
+      <ExpenseItem 
+      title= {expense[1].title}
+      amount={expense[1].amount}
+      date={expense[1].date}
+      location={expense[1].location}
+      ></ExpenseItem>
+
+      <ExpenseItem 
+      title= {expense[2].title}
+      amount={expense[2].amount}
+      date={expense[2].date}
+      location={expense[2].location}
+      ></ExpenseItem>
+
+      <ExpenseItem 
+      title= {expense[3].title}
+      amount={expense[3].amount}
+      date={expense[3].date}
+      location={expense[3].location}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,18 @@
 import "./ExpenseItem.css"
 
-function ExpenseItem(){
+function ExpenseItem(props){
 
-    const expenseDate = new Date(2021, 2 ,28);
-    const expenseTitle = 'Car insurence';
-    const expenseAmount = 100;
-    const expenseLocation = 'Banglore'
+   
 
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <h2>{expenseLocation}</h2>
+                <h2>{props.title}</h2>
+                <h2>{props.location}</h2>
 
-                <div className="expense-item__price">${expenseAmount}</div>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     )
