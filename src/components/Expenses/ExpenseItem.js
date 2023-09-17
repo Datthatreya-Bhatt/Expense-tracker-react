3c8c37b0f25b4f72ba04a19f8554fc4e3c22ed41
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-09-17 13:35:16 +0530

    eventlistener is created

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,12 +7,18 @@ import "./ExpenseItem.css"
 
 const ExpenseItem = (props) =>{
    
+    
+
+    const clickhandler = ()=>{
+        console.log('expense deleted')
+    }
 
 
     return (
         <div className="expense-item">
             <ExpenseDate date={props.date}/>
             <ExpenseDetail title={props.title} location={props.location} amount={props.amount}/>
+            <button onClick={clickhandler}>Delete Expense</button>
         </div>
     )
 }
