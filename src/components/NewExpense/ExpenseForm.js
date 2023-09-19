31ee1051dd5c3c72b843fc3dd23e32334b54073d
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-09-19 20:20:24 +0530

    forms button added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -39,6 +39,7 @@ const ExpenseForm = (props) => {
 
 
 
+
     return (
         <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
@@ -61,6 +62,7 @@ const ExpenseForm = (props) => {
             </div>
 
             <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancel} >Cancel</button>
                 <button type="submit" >Add Expense</button>
             </div>
 
