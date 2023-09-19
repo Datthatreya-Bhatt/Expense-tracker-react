cd3ef18b18ecad3288cf315cfe04ff8ff6edaa79
Datthatreya-Bhatt <datthatreyabhatt98@gmail.com>
2023-09-19 16:13:51 +0530

    users can add data dynamically

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {id: '1', title: 'A', amount: 500, date: new Date(2021, 2,28)},
+  {id: '2', title: 'B', amount: 300, date: new Date(2021, 2,28)},
+  {id: '3', title: 'C', amount: 200, date: new Date(2021, 2,28)},
+  {id: '4', title: 'D', amount: 10, date: new Date(2021, 2,28)},
+]
+
 function App() {
 
-  const expenses = [
-    {id: '1', title: 'Abc', amount: 500, date: new Date(2021, 2,28)},
-    {id: '2', title: 'efg', amount: 300, date: new Date(2021, 2,28)},
-    {id: '3', title: 'hij', amount: 200, date: new Date(2021, 2,28)},
-    {id: '4', title: 'klm', amount: 10, date: new Date(2021, 2,28)},
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  
 
   const addExpenseHandler = (expense)=>{
-    console.log('In App.js');
-    console.log(expense);
+      setExpenses( prevExpenses=>{
+        return [expense, ...prevExpenses];
+      })  
   }
 
   
